e3e6299c80be4f6062adc7c09058c3ae20b68070
Arthus <arthus@gfz-potsdam.de>
2021-10-13 11:25:37 +0200

    Centralized datDir and planetlist.

---
+++ planetmagfields/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+stdDatDir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/')
+planetlist = ["mercury", "earth", "jupiter", "saturn", "uranus", "neptune",
+              "ganymede"]
