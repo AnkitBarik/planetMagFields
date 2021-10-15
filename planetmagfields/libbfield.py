395e0b666b1ba420a896c33424683b0a4c7a656b
Arthus <arthus@gfz-potsdam.de>
2021-10-15 10:49:41 +0200

    Do not use get_data in functions and have them operate on planet instances instead.

--- planetmagfields/libbfield.py
+++ planetmagfields/libbfield.py
@@ -3,47 +3,45 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from .libgauss import get_data,gen_idx,get_grid,getB,getBm0
+from .planet import planet as Planet
+from .libgauss import get_grid,getB,getBm0
 from .plotlib import *
 from .utils import planetlist, stdDatDir
 
-import sys
 
-
-
-def getBr(datDir=stdDatDir,planet="earth",r=1,info=True):
-
-    if planet not in planetlist:
-        print("Planet must be one of the following!")
-        print(planetlist)
-        sys.exit()
-
-    if planet == '--help':
-        print("Usage: ./magField.py <planet name>")
-        print("Example: ./magField.py earth")
-        sys.exit()
-
-    g,h,lmax,idx = get_data(datDir,planet)
+def getBr(planet, r=1, info=True):
 
     p2D,th2D = get_grid()
 
-    if planet in ["mercury","saturn"]:
-        Br = getBm0(lmax,g,r,p2D,th2D) * 1e-3
+    if planet.name in ["mercury", "saturn"]:
+        Br = getBm0(planet.lmax,
+                    planet.glm,
+                    r,
+                    p2D,
+                    th2D) * 1e-3
         dipTheta = 0.
         dipPhi = 0.
     else:
-        Br = getB(lmax,g,h,idx,r,p2D,th2D,planet=planet) * 1e-3
+        Br = getB(planet.lmax,
+                  planet.glm,
+                  planet.hlm,
+                  planet.idx,
+                  r,
+                  p2D,
+                  th2D,
+                  planet=planet.name) * 1e-3
 
         dipTheta = np.arctan(np.sqrt(g[idx[1,1]]**2 + h[idx[1,1]]**2)/g[idx[1,0]]) * 180./np.pi
         dipPhi = np.arctan(h[idx[1,1]]/g[idx[1,1]]) * 180./np.pi
 
     if info:
-        print(("Planet: %s" %planet.capitalize()))
+        print(("Planet: %s" %planet.name.capitalize()))
         #print(("Depth (fraction of surface radius) = %.2f" %r))
-        print(("l_max = %d" %lmax))
+        print(("l_max = %d" %planet.lmax))
         print(("Dipole tilt (degrees) = %f" %dipTheta))
 
     return p2D, th2D, Br, dipTheta, dipPhi
+
 
 def plotAllFields(datDir=stdDatDir,r=1.0,levels=30,cmap='RdBu_r',proj='Mollweide'):
 
@@ -53,10 +51,11 @@ def plotAllFields(datDir=stdDatDir,r=1.0,levels=30,cmap='RdBu_r',proj='Mollweide
     print('|=========|======|=======|')
 
     plt.figure(figsize=(12,12))
-    for k, planet in enumerate(planetlist):
-        p2D,th2D,Br,dipTheta,dipPhi = getBr(datDir=datDir,planet=planet,r=r,info=False)
+    for k, name in enumerate(planetlist):
+        planet = Planet(name=name)
+        p2D,th2D,Br,dipTheta,dipPhi = getBr(planet=planet,r=r,info=False)
 
-        if planet == "ganymede":
+        if name == "ganymede":
             nplot = 8
         else:
             nplot = k+1
@@ -68,7 +67,7 @@ def plotAllFields(datDir=stdDatDir,r=1.0,levels=30,cmap='RdBu_r',proj='Mollweide
             projection = eval('ccrs.'+proj+'()')
             ax = plt.subplot(3,3,nplot,projection=projection)
 
-        plotB_subplot(p2D,th2D,Br,ax,planet=planet,levels=levels,cmap=cmap,proj=proj)
+        plotB_subplot(p2D,th2D,Br,ax,planet=name,levels=levels,cmap=cmap,proj=proj)
 
         if planet in ["mercury","saturn"]:
             print(('|%-8s | %-4.1f | %-5.1f |' %(planet.capitalize(),dipTheta, dipPhi)))
@@ -83,10 +82,8 @@ def plotAllFields(datDir=stdDatDir,r=1.0,levels=30,cmap='RdBu_r',proj='Mollweide
         plt.suptitle(r'Radial magnetic field ($\mu$T) at $r/r_{\rm surface} = %.2f$' %r, fontsize=20)
 
 
-
-
-def plotMagField(datDir=stdDatDir,planet="earth",r=1,levels=30,proj='moll',cmap='RdBu_r'):
-
-    p2D, th2D, Br, dum1,dum2 = getBr(datDir=datDir,planet=planet,r=r)
+def plotMagField(name,r=1,levels=30,proj='moll',cmap='RdBu_r'):
+    planet = Planet(name)
+    p2D, th2D, Br, dum1,dum2 = getBr(planet=planet,r=r)
     plt.figure(figsize=(12,6.75))
     plotB(p2D,th2D,Br,planet=planet,levels=levels,proj=proj,cmap=cmap,r=r)
