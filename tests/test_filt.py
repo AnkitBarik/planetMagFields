dc41464b08410cf5aec45fc208661f0472445aaf
AnkitBarik <ankit.barik@gmail.com>
2025-02-09 22:11:33 -0500

    Try to reduce resources

--- tests/test_filt.py
+++ tests/test_filt.py
@@ -13,6 +13,8 @@ def test_filt_sat():
 
     percent_err = np.abs( (br_ref - br)/br_ref ) * 100
 
+    del p
+
     np.testing.assert_allclose(percent_err, 0, rtol=0.1,
                                 atol=0.1)
 
@@ -25,17 +27,21 @@ def test_filt_earth():
 
     percent_err = np.abs( (br_ref - br)/br_ref ) * 100
 
+    del p
+
     np.testing.assert_allclose(percent_err, 0, rtol=0.1,
                                 atol=0.1)
 
 def test_filt_consistent():
     from copy import deepcopy
 
-    pl = Planet(name="earth",year=2020,nphi=256,info=False,model='igrf14')
-    pl.plot_filt(lCutMax=pl.lmax,iplot=False)
-    brfilt1 = deepcopy(pl.Br_filt)
-    pl.plot_filt(lCutMin=0,iplot=False)
-    brfilt2 = deepcopy(pl.Br_filt)
+    p = Planet(name="earth",year=2020,nphi=128,info=False,model='igrf14')
+    p.plot_filt(lCutMax=p.lmax,iplot=False)
+    brfilt1 = deepcopy(p.Br_filt)
+    p.plot_filt(lCutMin=0,iplot=False)
+    brfilt2 = deepcopy(p.Br_filt)
     err = brfilt1 - brfilt2
+
+    del p
 
     np.testing.assert_allclose(err, 0, rtol=1e-12, atol=1e-12)
