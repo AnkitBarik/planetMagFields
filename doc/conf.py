97fa923721cfc809b19b4ed0b41595eafca9bdba
AnkitBarik <ankit.barik@gmail.com>
2025-05-14 19:20:26 -0400

    Update patch version

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'planetMagFields'
 copyright = '2025, Ankit Barik'
 author = 'Ankit Barik'
-release = '1.5.5'
+release = '1.5.6'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
